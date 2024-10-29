c783e904c03735065e2b4947446e7bd97402684f
Lukas Olivier <olivier.lukas2003@gmail.com>
2024-10-29 18:08:20 +0100

    added email validation

--- components/verification-drawer/Information.vue
+++ components/verification-drawer/Information.vue
@@ -11,15 +11,64 @@
       <template #content>
          <form @submit.prevent="handleNext">
             <div class="max-h-[300px] space-y-4 overflow-scroll">
-               <input type="text" placeholder="First name" class="form-input" />
-               <input type="text" placeholder="Last name" class="form-input" />
-               <input type="email" placeholder="Email" class="form-input" />
-               <input type="text" placeholder="Address" class="form-input" />
-               <input type="text" placeholder="City" class="form-input" />
-               <input type="text" placeholder="State" class="form-input" />
-               <!-- Zip Code and country -->
-               <input type="text" placeholder="Zip Code" class="form-input" />
-               <input type="text" placeholder="Country" class="form-input" />
+               <input
+                  v-model="firstName"
+                  type="text"
+                  placeholder="First name"
+                  class="form-input"
+                  required
+               />
+               <input
+                  v-model="lastName"
+                  type="text"
+                  placeholder="Last name"
+                  class="form-input"
+                  required
+               />
+               <input
+                  v-model="email"
+                  type="email"
+                  placeholder="Email"
+                  class="form-input"
+                  :class="{ 'invalid-input': noValidEmail }"
+                  required
+                  @blur="validateEmail"
+               />
+               <input
+                  v-model="address"
+                  type="text"
+                  placeholder="Address"
+                  class="form-input"
+                  required
+               />
+               <input
+                  v-model="city"
+                  type="text"
+                  placeholder="City"
+                  class="form-input"
+                  required
+               />
+               <input
+                  v-model="state"
+                  type="text"
+                  placeholder="State"
+                  class="form-input"
+                  required
+               />
+               <input
+                  v-model="zipCode"
+                  type="text"
+                  placeholder="Zip Code"
+                  class="form-input"
+                  required
+               />
+               <input
+                  v-model="country"
+                  type="text"
+                  placeholder="Country"
+                  class="form-input"
+                  required
+               />
             </div>
             <!-- Navigation Buttons -->
             <div class="mt-6 flex justify-center gap-4">
@@ -38,8 +87,9 @@
                   class="px-5 py-3 transition-all duration-200"
                   :class="{
                      'cursor-not-allowed bg-gray-100 opacity-50 hover:bg-gray-100':
-                        !isCompleteAndCorrect,
+                        !isCompleteAndCorrect || noValidEmail,
                   }"
+                  :disabled="!isCompleteAndCorrect || noValidEmail"
                   @click="handleNext"
                >
                   <template #icon-append>
@@ -54,31 +104,77 @@
 
 <script setup>
 import { PhArrowLeft, PhArrowRight } from "@phosphor-icons/vue";
+
 const showDrawer = ref(true);
 const emit = defineEmits(["close", "next", "back"]);
-
 const props = defineProps({
    show: {
       type: Boolean,
       default: false,
    },
 });
-
 watchEffect(() => {
    showDrawer.value = props.show;
 });
+
+const firstName = ref("");
+const lastName = ref("");
+const email = ref("");
+const address = ref("");
+const city = ref("");
+const state = ref("");
+const zipCode = ref("");
+const country = ref("");
+
+const isCompleteAndCorrect = computed(() => {
+   return (
+      firstName.value &&
+      lastName.value &&
+      email.value &&
+      address.value &&
+      city.value &&
+      state.value &&
+      zipCode.value &&
+      country.value
+   );
+});
+
+const isValidEmail = (email) => {
+   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+   return emailRegex.test(email);
+};
+
+const noValidEmail = ref(false);
 
 const handleCloseDrawer = () => {
    emit("close");
 };
 
 const handleNext = () => {
-   emit("next");
+   if (!isValidEmail(email.value)) {
+      noValidEmail.value = true;
+      return;
+   }
+   if (isCompleteAndCorrect.value) {
+      emit("next");
+   }
+};
+
+const handleBack = () => {
+   emit("back");
+};
+
+const validateEmail = () => {
+   noValidEmail.value = !!email.value && !isValidEmail(email.value);
 };
 </script>
 
 <style scoped>
 .form-input {
-   @apply w-full rounded-lg border border-gray-300 px-2 py-4 focus:outline-none focus:ring-0;
+   @apply w-full rounded-lg border border-gray-300 px-2 py-4 focus:border-resin-500 focus:outline-none focus:ring-0;
+}
+
+.invalid-input {
+   @apply w-full rounded-lg border-2 border-red-500 px-2 py-4 focus:outline-none focus:ring-0 !important;
 }
 </style>
