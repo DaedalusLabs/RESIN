e9d982000898c3a05ddb42121e2636669d6c5c20
Lukas Olivier <olivier.lukas2003@gmail.com>
2024-11-08 12:13:14 +0100

    feat: base layout done

---
+++ components/my-resin/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+   <nav class="flex items-center justify-between">
+      <h2 class="text-lg font-semibold text-pirate-800">{{ title }}</h2>
+      <NuxtLink :to="localePath(link)">
+         <PhArrowRight :size="20" weight="bold" class="text-resin-500" />
+      </NuxtLink>
+   </nav>
+</template>
+
+<script setup>
+import { PhArrowRight } from "@phosphor-icons/vue";
+
+defineProps({
+   title: {
+      type: String,
+      required: true,
+   },
+   link: {
+      type: String,
+      required: true,
+   },
+});
+</script>
---
+++ pages/my-resin.vue
@@ -0,0 +1,32 @@
+<template>
+   <section class="mx-auto flex w-10/12 flex-col py-20">
+      <h1 class="mb-10 text-2xl font-extrabold leading-tight">My Resin</h1>
+
+      <div class="space-y-10">
+         <section class="flex flex-col gap-4">
+            <MyResinNavbar title="Properties" link="my-properties" />
+            <!-- TODO: Add properties -->
+         </section>
+         <section>
+            <MyResinNavbar title="Transactions" link="my-transactions" />
+            <!-- TODO: Add transactions -->
+         </section>
+         <section>
+            <MyResinNavbar title="Agreements" link="my-agreements" />
+            <!-- TODO: Add agreements -->
+         </section>
+         <section>
+            <MyResinNavbar title="Financials" link="my-financials" />
+            <!-- TODO: Add finances -->
+         </section>
+      </div>
+   </section>
+</template>
+
+<script setup>
+import { usePropertiesStore } from "~/stores/properties";
+
+definePageMeta({
+   layout: "white",
+});
+</script>
