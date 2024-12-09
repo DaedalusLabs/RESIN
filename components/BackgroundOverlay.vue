b5818708e7eab98847fddf4dfc33868ba3b4901d
Lukas Olivier <olivier.lukas2003@gmail.com>
2024-12-09 22:59:51 +0100

    completed imagedrawer rework

---
+++ components/BackgroundOverlay.vue
@@ -0,0 +1,31 @@
+<script setup>
+defineProps({
+   show: {
+      type: Boolean,
+      required: true,
+   },
+});
+
+const emit = defineEmits(["close"]);
+</script>
+
+<template>
+   <transition name="fade" appear>
+      <div
+         v-if="show"
+         class="z-top fixed inset-0 w-full bg-black bg-opacity-50"
+         @click.self="emit('close')"
+      />
+   </transition>
+</template>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+   transition: opacity 0.3s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+   opacity: 0;
+}
+</style>
--- components/Flowbite/ImageDrawer.vue
+++ components/Flowbite/ImageDrawer.vue
@@ -14,69 +14,60 @@ const emit = defineEmits(["close"]);
 </script>
 
 <template>
-   <transition name="fade" appear>
+   <transition name="fade-slide" appear>
       <div
          v-if="showDrawer"
-         class="fixed inset-0 z-40 flex items-end bg-black bg-opacity-50 transition-opacity"
+         class="z-top fixed inset-0 flex h-full w-full items-end"
          @click.self="emit('close')"
       >
-         <transition name="slide-up" appear>
-            <div
-               class="w-full overflow-y-auto bg-white dark:bg-gray-800 lg:min-h-[75vh] lg:self-end"
-            >
-               <div class="sticky top-0 z-50">
-                  <span
-                     class="absolute left-1/2 top-3 h-1 w-10 -translate-x-1/2 cursor-pointer rounded-lg bg-gray-500"
-                     @click="emit('close')"
-                  />
-               </div>
-               <div class="grid grid-cols-1 lg:grid-cols-2">
-                  <div
-                     v-if="imageUrls.length === 0"
-                     class="col-span-full text-center text-gray-500"
-                  >
-                     No images to display
-                  </div>
-                  <NuxtImg
-                     v-for="(imageUrl, index) in imageUrls"
-                     :key="index"
-                     :src="imageUrl"
-                     :class="{
-                        'col-span-2':
-                           imageUrls.length % 2 !== 0 &&
-                           index === imageUrls.length - 1,
-                     }"
-                     class="h-64 w-full rounded-md object-cover shadow-lg lg:h-96"
-                     loading="lazy"
-                  />
-               </div>
+         <div
+            :class="[
+               'max-h-[80vh] w-full bg-white lg:self-end dark:bg-gray-800',
+               { 'overflow-y-auto': imageUrls.length > 1 },
+            ]"
+         >
+            <div class="sticky top-0 z-50">
+               <span
+                  class="absolute left-1/2 top-3 h-1 w-10 -translate-x-1/2 cursor-pointer rounded-lg bg-gray-500"
+                  @click="emit('close')"
+               />
             </div>
-         </transition>
+            <div class="grid grid-cols-1 lg:grid-cols-2">
+               <div
+                  v-if="imageUrls.length === 0"
+                  class="col-span-full text-center text-gray-500"
+               >
+                  No images to display
+               </div>
+               <NuxtImg
+                  v-for="(imageUrl, index) in imageUrls"
+                  :key="index"
+                  :src="imageUrl"
+                  :class="{
+                     'col-span-2':
+                        imageUrls.length % 2 !== 0 &&
+                        index === imageUrls.length - 1,
+                  }"
+                  class="max-h-[50vh] w-full rounded-md object-cover object-center shadow-lg"
+                  loading="lazy"
+               />
+            </div>
+         </div>
       </div>
    </transition>
 </template>
 
 <style>
-.slide-up-enter-active,
-.slide-up-leave-active {
+.fade-slide-enter-active,
+.fade-slide-leave-active {
    transition:
-      transform 0.3s ease,
-      opacity 0.3s ease;
+      opacity 0.3s ease,
+      transform 0.3s ease;
 }
 
-.slide-up-enter-from,
-.slide-up-leave-to {
+.fade-slide-enter-from,
+.fade-slide-leave-to {
+   opacity: 0;
    transform: translateY(100%);
-   opacity: 0;
-}
-
-.fade-enter-active,
-.fade-leave-active {
-   transition: opacity 0.3s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-   opacity: 0;
 }
 </style>
