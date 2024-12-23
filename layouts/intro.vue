e836d58e90e721c45d52dc76296de9b685af510c
Lukas Olivier <olivier.lukas2003@gmail.com>
2024-12-23 15:15:56 +0100

    refactor: desktop image is not fetched when viewing on mobile

--- layouts/intro.vue
+++ layouts/intro.vue
@@ -1,13 +1,28 @@
+<script setup>
+const isDesktop = ref(false);
+
+onMounted(() => {
+   isDesktop.value = window.innerWidth >= 1024; // same as lg breakpoint from tailwind
+});
+</script>
+
 <template>
    <div
       class="flex h-screen flex-col bg-pirate-950 bg-hex-dark bg-cover bg-center"
    >
       <main class="flex h-full flex-1">
-         <NuxtImg
-            class="hidden h-full w-3/5 object-cover lg:block"
-            src="/images/introduction-desktop.png"
-            alt="background"
-         />
+         <div class="hidden h-full w-3/5 lg:block">
+            <NuxtImg
+               v-if="isDesktop"
+               class="h-full w-full object-cover"
+               src="/images/introduction-desktop.png"
+               alt="background"
+               placeholder
+               loading="lazy"
+               width="50vw"
+               height="100vw"
+            />
+         </div>
          <div class="h-full flex-1">
             <slot />
          </div>
