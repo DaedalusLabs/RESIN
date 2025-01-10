e1c46a35e3a0ca6bf54751204559eab158f59584
Djuri Baars <dsbaars@users.noreply.github.com>
2025-01-10 16:10:15 +0100

    fix: Improve extension detection

--- components/Flowbite/BorderButton.vue
+++ components/Flowbite/BorderButton.vue
@@ -1,7 +1,7 @@
 <template>
    <button
       type="button"
-      class="inline-flex items-center justify-center rounded-lg border border-resin-500 bg-transparent px-4 py-2 text-center text-sm font-medium text-resin-500 hover:bg-resin-500 hover:text-white focus:outline-none focus:ring-4 focus:ring-pirate-100"
+      :class="`inline-flex items-center justify-center rounded-lg border border-${color} bg-transparent px-4 py-2 text-center text-sm font-medium text-${color} hover:bg-${color} hover:text-${hoverTextColor} focus:outline-none focus:ring-4 focus:ring-pirate-100`"
    >
       <span class="mr-2">
          <slot name="icon-prepend" />
@@ -16,10 +16,18 @@
 </template>
 
 <script setup>
-defineProps({
+const props = defineProps({
    text: {
       type: String,
       required: true,
+   },
+   color: {
+      type: String,
+      default: 'resin-500',
+   },
+   hoverTextColor: {
+      type: String,
+      default: 'white',
    },
 });
 </script>
--- components/introduction/LoginModal.vue
+++ components/introduction/LoginModal.vue
@@ -2,34 +2,33 @@
    <FlowbiteModal :is-open="isModalOpen" @update:is-open="handleModalUpdate">
       <template #title>{{ $t('introduction.login.title') }}</template>
       <p class="my-6 text-sm">{{ $t('introduction.login.description') }}</p>
-      <div class="mt-4 flex w-full flex-col gap-4">    
-         <FlowbiteBorderButton
-            :text="$t('introduction.login.recoveryPhrase.button')"
-            @click="() => {
+      <div class="mt-4 flex w-full flex-col gap-4">
+         <FlowbiteBorderButton :text="$t('introduction.login.recoveryPhrase.button')" @click="() => {
                emit('openPhraseDrawer');
                handleModalUpdate(false);
-            }"
-         />
+            }" />
 
-         <FlowbiteBorderButton
-            v-if="hasNip44()"
-            :text="hasExtension() ? $t('introduction.login.extension.useExtension') : $t('introduction.login.extension.noExtension')" 
-            class="w-full border-gray text-gray" 
-            @click="hasExtension() ? handleExtensionLogin() : openExtensionApps()" 
-         />
-         <button 
-            v-else-if="!hasNip44()" 
-            class="inline-flex items-center justify-center rounded-lg border border-pirate-500 bg-transparent px-4 py-2 text-center text-sm font-medium text-pirate-500" 
-            disabled
-         >
-            {{ $t('introduction.login.extension.noNip44') }}
-         </button>
+         <section v-if="hasExtension()">
+            <FlowbiteBorderButton v-if="hasNip44()"
+               :text="hasExtension() ? $t('introduction.login.extension.useExtension') : $t('introduction.login.extension.noExtension')"
+               class="w-full border-gray text-gray"
+               @click="hasExtension() ? handleExtensionLogin() : openExtensionApps()" />
+            <button v-else-if="!hasNip44()"
+               class="w-full inline-flex items-center justify-center rounded-lg border border-pirate-500 bg-transparent px-4 py-2 text-center text-sm font-medium text-pirate-500"
+               disabled>
+               {{ $t('introduction.login.extension.noNip44') }}
+            </button>
+         </section>
+         <section v-else>
+            <FlowbiteBorderButton :text="$t('introduction.login.extension.noExtension')" class="w-full " color="gray-500" hoverTextColor="gray-500"  @click="openExtensionApps()" />
+         </section>
       </div>
    </FlowbiteModal>
 </template>
 
 <script setup>
 import { useNostr } from '~/composables/useNostr';
+import DisabledButton from '../Flowbite/DisabledButton.vue';
 const { loginWithExtension, isAuthenticated, hasExtension, hasNip44, checkAuthenticated } = useNostr();
 
 const emit = defineEmits(["openNsecDrawer", "openPhraseDrawer", "close"]);
